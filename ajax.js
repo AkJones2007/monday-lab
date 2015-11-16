d725afa882b4f062fc51001ab53e4194f2b528a0
Alex <AkJones2007@gmail.com>
2015-11-16 10:07:43 -0500

    Adds AJAX config to the API object to allow a callback to be executed to handle the received data

--- ajax.js
+++ ajax.js
@@ -2,4 +2,12 @@ var api = {
   // Set URL
   url: "http://jsonplaceholder.typicode.com/",
 
+  ajax: function(config, cb) {
+    $.ajax(config).done(function(data, textStatus, jqxhr) {
+      cb(null, data);
+    }).fail(function(jqxhr, status, error) {
+      cb({jqxher: jqxhr, status: status, error: error});
+    });
+  }
+
 };
